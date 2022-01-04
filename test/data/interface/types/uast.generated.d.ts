eae859b5e74692feb198cee5b21317f31d472d19
corno <gitkraken.a@schraverus.com>
2022-01-04 10:31:42 +0100

    .

--- test/data/interface/types/uast.generated.d.ts
+++
@@ -1,7 +0,0 @@
-import * as pr from "pareto-runtime";
-export declare type TUntypedNode<Annotation> = {
-    readonly "kindName": string;
-    readonly "value": string;
-    readonly "annotation": Annotation;
-    readonly "children": pr.IReadonlyArray<Node<Annotation>>;
-};
